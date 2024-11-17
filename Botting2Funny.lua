fb25f0de1cfce2ef47ee2356019d594cb2e6f251
2Sh-dy <biz.bankole.5@googlemail.com>
2024-11-17 17:34:09 +0000

    Add files via upload

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -2,10 +2,10 @@ local whitelist = {
     "falllostgravity",
     "bigbubbly_noob"
   }
-
   local px = "."
   local players = game:GetService("Players")
   local localplayer = players.LocalPlayer
+  local orbit
   for _,v in pairs(whitelist) do
       for _,p in pairs(players:GetPlayers()) do
           if string.lower(p.Name) == string.lower(v) then
@@ -13,14 +13,29 @@ local whitelist = {
                   msg = string.split(string.lower(msg), " ")
                   if msg[1] == px.."leave" then
                       players.LocalPlayer:Kick()
-                  elseif msg[1] == px.."prefix" then
-                      px = msg[2]
                   elseif msg[1] == px.."bring" then
                       localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
-                  elseif msg[1] == px.."say" then
-                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg[2])
                   elseif msg[1] == px.."orbit" then
-                    --insert orbit code here
+                      task.spawn(function()
+                      local origin = p.Character.HumanoidRootPart
+                      local r = msg[3] or 10;
+                      local rps = msg[2] or math.pi;
+                      local orbiter = localplayer.Character.HumanoidRootPart
+  
+                      local angle = 0;
+                      orbit = game:GetService'RunService'.Heartbeat:Connect(function(dt)
+                          angle = (angle + dt * rps) % (2 * math.pi);
+                          orbiter.CFrame = origin.CFrame * CFrame.new(math.cos(angle) * r, 0, math.sin(angle) * r); 
+                      end)
+                      end)
+                  elseif msg[1] == px.."unorbit" then
+                      orbit:Disconnect()
+                  elseif msg[1] == px.."walkto" or "w2" then
+                    local Humanoid = p
+                    for _,part in pairs(p.HumanoidRootPart) do
+                    Humanoid:MoveTo(part.Position)
+                    Humanoid.MoveToFinished:Wait()
+                    end
                   end
               end)
           end
