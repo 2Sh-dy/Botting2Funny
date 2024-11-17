0e2df3307ffb16b399886aff235d1ca4a48f555f
InternetDLL <127987847+InternetDLL@users.noreply.github.com>
2024-11-17 16:35:52 +0000

    Update Botting2Funny.lua

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -1,1 +1,16 @@
-
+local whitelist = {
+  "falllostgravity",
+  "bigbubbly_noob"
+}
+local players = game:GetService("Players")
+for _,v in pairs(whitelist) do
+  for _,p in pairs(players:GetPlayers()) do
+    if string.lower(p.Name) == string.lower(v) then
+      p.Chatted:Connect(function(msg)
+        if msg == "leave" then
+          players.LocalPlayer:Kick()
+        end
+      end)
+    end
+  end
+end
