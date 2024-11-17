57efd7c01228356b7dd3dfb8429847b633cace72
2Sh-dy <biz.bankole.5@googlemail.com>
2024-11-17 17:33:37 +0000

    Add files via upload

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -1,40 +1,28 @@
 local whitelist = {
-  "falllostgravity",
-  "bigbubbly_noob"
-}
-local px = "."
-local players = game:GetService("Players")
-local localplayer = players.LocalPlayer
-local orbit
-local function cmd(p)
-    p.Chatted:Connect(function(msg)
-        msg = string.split(string.lower(msg), " ")
-        if msg[1] == px.."leave" then
-            players.LocalPlayer:Kick()
-        elseif msg[1] == px.."bring" then
-            localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
-        elseif msg[1] == px.."orbit" then
-            task.spawn(function()
-            local origin = p.Character.HumanoidRootPart
-            local r = msg[3] or 10
-               local rps = math.pi
-            local orbiter = localplayer.Character.HumanoidRootPart
+    "falllostgravity",
+    "bigbubbly_noob"
+  }
 
-            local angle = 0;
-            orbit = game:GetService'RunService'.Heartbeat:Connect(function(dt)
-                angle = (angle + (dt/5)*(msg[2]/5) * rps) % (2 * math.pi);
-                orbiter.CFrame = origin.CFrame * CFrame.new(math.cos(angle) * r, 0, math.sin(angle) * r); 
-            end)
-            end)
-        elseif msg[1] == px.."unorbit" then
-            orbit:Disconnect()
-        end
-    end)
-end
-for _,v in pairs(whitelist) do
-    for _,p in pairs(players:GetPlayers()) do
-        if string.lower(p.Name) == string.lower(v) then
-            cmd(p)
-        end
-    end
-end
+  local px = "."
+  local players = game:GetService("Players")
+  local localplayer = players.LocalPlayer
+  for _,v in pairs(whitelist) do
+      for _,p in pairs(players:GetPlayers()) do
+          if string.lower(p.Name) == string.lower(v) then
+              p.Chatted:Connect(function(msg)
+                  msg = string.split(string.lower(msg), " ")
+                  if msg[1] == px.."leave" then
+                      players.LocalPlayer:Kick()
+                  elseif msg[1] == px.."prefix" then
+                      px = msg[2]
+                  elseif msg[1] == px.."bring" then
+                      localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
+                  elseif msg[1] == px.."say" then
+                    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg[2])
+                  elseif msg[1] == px.."orbit" then
+                    --insert orbit code here
+                  end
+              end)
+          end
+      end
+  end
