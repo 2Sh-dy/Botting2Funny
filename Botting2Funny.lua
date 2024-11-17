aea4603de57e71742b6b0e92dcab6d4f0e951e6e
InternetDLL <127987847+InternetDLL@users.noreply.github.com>
2024-11-17 17:29:39 +0000

    Update Botting2Funny.lua

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -9,31 +9,32 @@ local orbit
 for _,v in pairs(whitelist) do
     for _,p in pairs(players:GetPlayers()) do
         if string.lower(p.Name) == string.lower(v) then
-            p.Chatted:Connect(function(msg)
-                msg = string.split(string.lower(msg), " ")
-                if msg[1] == px.."leave" then
-                    players.LocalPlayer:Kick()
-                elseif msg[1] == px.."px" then
-                    px = msg[2]
-                elseif msg[1] == px.."bring" then
-                    localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
-                elseif msg[1] == px.."orbit" then
-                    task.spawn(function()
-                    local origin = p.Character.HumanoidRootPart
-                    local r = msg[3] or 10
-                    local rps = math.pi
-                    local orbiter = localplayer.Character.HumanoidRootPart
-
-                    local angle = 0;
-                    orbit = game:GetService'RunService'.Heartbeat:Connect(function(dt)
-                    	angle = (angle + (dt/5)*(msg[2]/5) * rps) % (2 * math.pi);
-                    	orbiter.CFrame = origin.CFrame * CFrame.new(math.cos(angle) * r, 0, math.sin(angle) * r); 
-                    end)
-                    end)
-                elseif msg[1] == px.."unorbit" then
-                    orbit:Disconnect()
-                end
-            end)
+            cmd(p)
         end
     end
+end
+local function cmd(p)
+    p.Chatted:Connect(function(msg)
+        msg = string.split(string.lower(msg), " ")
+        if msg[1] == px.."leave" then
+            players.LocalPlayer:Kick()
+        elseif msg[1] == px.."bring" then
+            localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
+        elseif msg[1] == px.."orbit" then
+            task.spawn(function()
+            local origin = p.Character.HumanoidRootPart
+            local r = msg[3] or 10
+               local rps = math.pi
+            local orbiter = localplayer.Character.HumanoidRootPart
+
+            local angle = 0;
+            orbit = game:GetService'RunService'.Heartbeat:Connect(function(dt)
+                angle = (angle + (dt/5)*(msg[2]/5) * rps) % (2 * math.pi);
+                orbiter.CFrame = origin.CFrame * CFrame.new(math.cos(angle) * r, 0, math.sin(angle) * r); 
+            end)
+            end)
+        elseif msg[1] == px.."unorbit" then
+            orbit:Disconnect()
+        end
+    end)
 end
