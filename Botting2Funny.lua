965bdbce377f379bc23e3b187a445534b5211897
InternetDLL <127987847+InternetDLL@users.noreply.github.com>
2024-11-17 19:30:47 +0000

    commands (and mrtopskyp)

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -1,3 +1,36 @@
+--[[
+--prefixes
+#plr -- targets plr, can be shortened
+all -- targets every player
+others -- targets every player excluding command user
+unwhitelisted -- targets every player excluding whitelisted users
+whitelisted -- targets every whitelisted user
+#team -- targets every player in a team
+nearest -- targets closest player to command user
+farthest -- targets farthest player to command user
+#range -- targets everybody within a range
+
+--commands
+leave
+ • bot gets kicked
+bring [plr]
+ • bot brings plr to command user
+ • if plr is nil, bot will teleport to command user
+goto [plr]
+ • bot brings command user to plr
+ • if plr is nil, bot will bring command user to bot
+fling [plr]
+ • bot flings plr
+ • if plr is nil, command will not run
+bounce [plr] [strength]
+ • bot teleports under plr and launches them upwards with strength
+ • if plr is nil, bot will launch command user
+ • to use strength, plr must be defined
+orbit [plr] [speed] [range]
+ • bot orbits plr with speed and range
+ • if plr is nil, bot will orbit command user
+ • if speed or range is nil, bot will use defaults
+]]
 local whitelist = {
   "falllostgravity",
   "bigbubbly_noob",
