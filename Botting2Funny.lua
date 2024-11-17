c7fc36ec2933027044c4a5f7700cc74635d2bd8c
InternetDLL <127987847+InternetDLL@users.noreply.github.com>
2024-11-17 16:50:01 +0000

    Update Botting2Funny.lua

--- Botting2Funny.lua
+++ Botting2Funny.lua
@@ -9,8 +9,13 @@ for _,v in pairs(whitelist) do
     for _,p in pairs(players:GetPlayers()) do
         if string.lower(p.Name) == string.lower(v) then
             p.Chatted:Connect(function(msg)
-                if msg == prefix.."leave" then
+                msg = string.split(string.lower(msg), " ")
+                if msg[1] == prefix.."leave" then
                     players.LocalPlayer:Kick()
+                elseif msg[1] == prefix.."prefix" then
+                    prefix = msg[2]
+                elseif msg[1] == prefix.."bring" then
+                    localplayer.Character.HumanoidRootPart.CFrame = p.Character.HumanoidRootPart.CFrame
                 end
             end)
         end
